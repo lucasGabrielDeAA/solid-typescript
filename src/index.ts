a3fa23234757abc69fd68323f0581716769b942d
lucasGabrielDeAA <lg.technus@gmail.com>
2020-04-15 11:58:29 -0300

    Examples

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import ShareButtonTwitter from "./ShareButtonTwitter";
+import ShareButtonFacebook from "./ShareButtonFacebook";
+import ShareButtonLinkedin from "./ShareButtonLinkedin";
+import ShareButtonPrint from "./ShareButtonPrint";
+import AbsctractShareButton from "./AbstractShareButton";
+import DOMEventHandler from "./DOMEventHandler";
+import MockEventHandler from "./MockEventHandler";
+
+const eventHandler = new DOMEventHandler();
+// const eventHandler = new MockEventHandler();
+
+const facebook: AbsctractShareButton = new ShareButtonFacebook("http://www.github.com/lucasGabrielDeAA", ".btn-facebook", eventHandler);
+facebook.bind();
+
+const twitter: AbsctractShareButton = new ShareButtonLinkedin("http://www.github.com/lucasGabrielDeAA", ".btn-twitter", eventHandler);
+twitter.bind();
+
+const linkedin: AbsctractShareButton = new ShareButtonTwitter("http://www.github.com/lucasGabrielDeAA", ".btn-linkedin", eventHandler);
+linkedin.bind();
+
+const print: AbsctractShareButton = new ShareButtonPrint(".print", eventHandler);
+print.bind();
